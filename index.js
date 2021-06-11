9a901144c5ee82a0d80f0e290057237969036501
Scott Bedard <scott.bedard@rakuten.com>
2021-06-11 09:06:54 -0700

    Update index.js
    add easter eggs

--- index.js
+++ index.js
@@ -12,13 +12,22 @@ var terms = [
     { term: "slave", suggestion: "replica" },
     { term: "whitelist", suggestion: "allowlist" },
     { term: "blacklist", suggestion: "denylist" },
+    { term: "white list", suggestion: "denylist" },
+    { term: "black list", suggestion: "denylist" },
     { term: "man hours", suggestion: "person hours" },
     { term: "sanity check", suggestion: "quick check" },
     { term: "dummy value", suggestion: "placeholder value" },
     { term: "master/slave", suggestion: "primary/replica" },
     { term: "spokesman", suggestion: "spokesperson" },
     { term: "spokeswoman", suggestion: "spokesperson" },
-    { term: "foreman", suggestion: "supervisor" }
+    { term: "foreman", suggestion: "supervisor" },
+    { term: "crazy", suggestion: "prone to bending reality" },
+    { term: "claire", suggestion: "prone to bending reality" }
+]
+
+var eggs = [
+    { term: "onewheel", suggestion: "Talking about Onewheels?  Scott must be in this channel, if not, you need tell him!" },
+    { term: "jeep", suggestion: "You all talking about Jeeps!  Is Dean here?  He should be." },
 ]
 
 slackEvents.on('message', (event) => {
@@ -27,18 +36,33 @@ slackEvents.on('message', (event) => {
     if (
         event.hasOwnProperty('text')
     ) {
+        //first process standard inclusive terms
         var suggested_replacement = terms.find(function(post, index) {
             if(event.text.toLowerCase().includes(post.term))
                 return post
         });
 
+        //process easter eggs with different response type
+        if (typeof suggested_replacement == 'undefined'){
+            var suggested_replacement = eggs.find(function(post, index) {
+                if(event.text.toLowerCase().includes(post.term))
+                    return post
+            });
+            if (typeof suggested_replacement !== 'undefined'){
+                var suggested_text = suggested_replacement.suggestion
+            }
+
+        } else {
+            var suggested_text = 'Hey, perhaps consider ' + suggested_replacement.suggestion + ' instead of _' + suggested_replacement.term + '_?'
+        }
+
         if (typeof suggested_replacement !== 'undefined'){
-            console.log("suggested:" + suggested_replacement.suggestion);
+            console.log("suggested:" + suggested_text);
             (async () => {
                 await web.chat.postEphemeral({
                 channel: event.channel,
                 user: event.user,
-                text: 'Hey, perhaps consider ' + suggested_replacement.suggestion + ' instead of _' + suggested_replacement.term + '_?',
+                text: suggested_text,
                 });
             })();        
         }
